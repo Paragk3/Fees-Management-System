be99ef3d1a0f3a721a35b92360d3f4ba80d3ea2c
username <user_khirade36@gmal.com>
2024-09-10 23:17:06 +0530

    create project on fees management

---
+++ main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.io.*;
+
+class feesReceipt{
+
+    public static void main(String args[]){
+        
+
+    }
+
+}
