921eb95a76fd3eb06f76479bc9ce538d48c1dc8a
username <user_khirade36@gmal.com>
2024-09-11 01:48:11 +0530

    Update container update ui buttons and textfields

--- main.java
+++ main.java
@@ -23,40 +23,46 @@ class feesReceipt{
 	static JButton printreceipt = new JButton("PRINT RECEIPT");
 	static int num =0;
 
-    public static int getNum() {
-        return num;
-    }
-
-    public static void setNum(int num) {
-        feesReceipt.num = num;
-    }
-
+    
     public static void main(String args[]){
-        addnewframe.setSize(800, 800);
-        addnewframe.setLayout(null);
-        addnewframe.setVisible(true);
-        addnewframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        addnewframe.setTitle("Fees Receipt");
-        addnewframe.add(amounttext);
-        addnewframe.add(snametext);
-        addnewframe.add(sclasstext);
-        addnewframe.add(fnametext);
-        addnewframe.add(fmobiletext);
-        addnewframe.add(mnametext);
-        addnewframe.add(mmobiletext);
-        addnewframe.add(saddresstext);
-        addnewframe.add(smobiletext);
-        addnewframe.add(dojtext);
-        addnewframe.add(receiptarea);
-        addnewframe.add(reset);
-        addnewframe.add(generatereceipt);
-        addnewframe.add(printreceipt);
-        addnewframe.add(new JLabel("Amount:"));
-        addnewframe.add(new JLabel("Student Name:"));
-        addnewframe.add(new JLabel("Class:"));
-        addnewframe.add(new JLabel("Father's Name:"));
-        addnewframe.add(new JLabel("Father's Mobile:"));
-        addnewframe.add(new JLabel("Mother's Name:"));
+        JPanel header = new JPanel();
+		JLabel headerlabel = new JLabel("FEES RECEIPT SYSTEM");
+		JPanel sname = new JPanel();
+		JLabel snamelabel = new JLabel("STUDENT NAME : ");
+		JPanel sclass = new JPanel();
+		JLabel sclasslabel = new JLabel("ENTER CLASS : ");
+		JPanel fname = new JPanel();
+		JLabel fnamelabel = new JLabel("FATHER NAME : ");
+		JPanel fmobile = new JPanel();
+		JLabel fmobilelabel = new JLabel("FATHER MOB : ");
+		JPanel mname = new JPanel();
+		JLabel mnamelabel = new JLabel("MOTHER NAME : ");
+		JPanel mmobile = new JPanel();
+		JLabel mmobilelabel = new JLabel("MOTHER MOB : ");
+		JPanel saddress = new JPanel();
+		JLabel saddresslabel = new JLabel("ENTER ADDRESS : ");
+		JPanel smobile = new JPanel();
+		JLabel smobilelabel = new JLabel("STUDENT MOB : ");
+		JPanel doj = new JPanel();
+		JLabel dojlabel = new JLabel("DATE OF JOINING : ");
+		JPanel amount = new JPanel();
+		JLabel amountlabel = new JLabel("FEE AMOUNT : ");
+	
+		
+		addnewframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		addnewframe.setBounds(0,0,1375,725);
+		addnewframe.setVisible(true);
+		
+		
+		Container cntr = addnewframe.getContentPane();
+		cntr.setLayout(null);
+		cntr.setBackground(Color.CYAN);
+		cntr.add(header);
+		header.setLayout(null);
+		header.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		header.setBounds(300,0,750,100);
+		header.add(headerlabel);
+		headerlabel.setBounds(75,0,625,150);
         
 
 
